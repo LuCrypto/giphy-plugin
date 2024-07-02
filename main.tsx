58a2ac9fdc8607b04bb555f22417f6d776991f55
LuCrypto <lucjager67@gmail.com>
2024-07-02 07:50:24 +0200

    Fix

--- main.tsx
+++ main.tsx
@@ -13,33 +13,19 @@ import ReactDOM from "react-dom/client";
 
 import "./styles.css";
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
+interface GiphySettings {
   apiKey: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
+const DEFAULT_SETTINGS: GiphySettings = {
   apiKey: "",
 };
 
-export default class MyPlugin extends Plugin {
-  settings: MyPluginSettings;
+export default class Giphy extends Plugin {
+  settings: GiphySettings;
 
   async onload() {
     await this.loadSettings();
-
-    // This creates an icon in the left ribbon.
-    const ribbonIconEl = this.addRibbonIcon(
-      "dice",
-      "Sample Plugin",
-      (evt: MouseEvent) => {
-        // Called when the user clicks the icon.
-        new Notice("This is a notice!");
-      }
-    );
-    // Perform additional things with the ribbon
-    ribbonIconEl.addClass("my-plugin-ribbon-class");
 
     // This adds a simple command that can be triggered anywhere
     this.addCommand({
@@ -86,18 +72,7 @@ export default class MyPlugin extends Plugin {
     });
 
     // This adds a settings tab so the user can configure various aspects of the plugin
-    this.addSettingTab(new SampleSettingTab(this.app, this));
-
-    // If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-    // Using this function will automatically remove the event listener when this plugin is disabled.
-    this.registerDomEvent(document, "click", (evt: MouseEvent) => {
-      // console.log("click", evt);
-    });
-
-    // When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-    this.registerInterval(
-      window.setInterval(() => console.log("setInterval"), 5 * 60 * 1000)
-    );
+    this.addSettingTab(new GiphySettingTab(this.app, this));
   }
 
   onunload() {}
@@ -248,26 +223,10 @@ const GifComponentSearch: React.FC<{
   );
 };
 
-class SampleModal extends Modal {
-  constructor(app: App) {
-    super(app);
-  }
+class GiphySettingTab extends PluginSettingTab {
+  plugin: Giphy;
 
-  onOpen() {
-    const { contentEl } = this;
-    contentEl.setText("Woah!");
-  }
-
-  onClose() {
-    const { contentEl } = this;
-    contentEl.empty();
-  }
-}
-
-class SampleSettingTab extends PluginSettingTab {
-  plugin: MyPlugin;
-
-  constructor(app: App, plugin: MyPlugin) {
+  constructor(app: App, plugin: Giphy) {
     super(app, plugin);
     this.plugin = plugin;
   }
